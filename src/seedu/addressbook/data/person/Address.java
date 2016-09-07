7a19f82f84d05284caafd94aa860abd98a72e2c9
Syed Abdullah <syed@syedabdullah.net>
2016-09-08 00:43:52 +0800

    Implement Printable in relevant classes and override required methods

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -6,7 +6,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
-public class Address {
+public class Address implements Printable {
 
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
@@ -55,4 +55,15 @@ public class Address {
     public boolean isPrivate() {
         return isPrivate;
     }
+
+	@Override
+	public String getPrintableString() {
+		final StringBuilder builder = new StringBuilder();
+		if (this.isPrivate) {
+			builder.append("(private) ");
+		}
+		builder.append("Address: ")
+			   .append(this.toString());
+		return builder.toString();
+	}
 }
