ee0f0e8b4c6a0d5414051c1c70fd55b72ac858ac
Syed Abdullah <syed@syedabdullah.net>
2016-09-08 11:35:11 +0800

    TDD: Modified Name class so that isSimilar_similarNames test case passes

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -61,4 +61,8 @@ public class Name {
         return fullName.hashCode();
     }
 
+    public boolean isSimilar(Name otherName) {
+        return (this.toString() == otherName.toString());
+    }
+
 }
